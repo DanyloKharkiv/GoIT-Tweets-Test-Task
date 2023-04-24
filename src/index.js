fdf31555d5a8cfd9626e86b9e08e28fbdc8f4d27
Danylo Kharkiv <donatelod12@gmail.com>
2023-04-25 00:13:14 +0300

    make some change

--- src/index.js
+++ src/index.js
@@ -10,13 +10,13 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="goit-tweets-test-task">
+    <BrowserRouter basename="goit-tweets-test-task">
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
           <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
