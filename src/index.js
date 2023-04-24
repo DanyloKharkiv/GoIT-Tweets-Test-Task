971b99ec4eb7fc453c21701ae6be7fccf102080c
Danylo Kharkiv <donatelod12@gmail.com>
2023-04-24 23:56:24 +0300

    try to fix 404

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/GoIT-Tweets-Test-Task">
+        <BrowserRouter basename="/goit-tweets-test-task">
           <App />
         </BrowserRouter>
       </PersistGate>
